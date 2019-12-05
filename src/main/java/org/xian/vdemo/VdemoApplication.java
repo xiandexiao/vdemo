d186a4221d4100d5abd1c76221071a82be3e37c9
xiandexiao <647887292@qq.com>
2019-12-05 16:47:43 +0800

    Initial springboot-demo

---
+++ src/main/java/org/xian/vdemo/VdemoApplication.java
@@ -0,0 +1,13 @@
+package org.xian.vdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/xian/vdemo/VdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xian.vdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
